2391e5405e6b734d5d9e9ac99d998e6cabfc8efb
Wizqqrl <baban1979molotov@gmail.com>
2024-11-22 03:13:00 +0600

    push

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,16 +1,14 @@
 import express from 'express';
 import { register, login, verifyCode, forgotPassword, resetPassword, getMe } from '../controllers/authController.js';
-import { generalLimiter, authLimiter } from '../utils/rateLimiter.js'
 import checkAuth from '../utils/checkAuth.js'
 const router = express.Router();
 
-router.use(generalLimiter);
 
-router.post('/register', authLimiter, register);
-router.post('/login', authLimiter, login);
-router.post('/verify-code', authLimiter, verifyCode);
-router.post('/reset-password', authLimiter, forgotPassword);
-router.post('/reset-password/confirm', authLimiter, resetPassword);
+router.post('/register', register);
+router.post('/login', login);
+router.post('/verify-code', verifyCode);
+router.post('/reset-password', forgotPassword);
+router.post('/reset-password/confirm', resetPassword);
 router.get('/getme',checkAuth, getMe  )
 
 export default router;
